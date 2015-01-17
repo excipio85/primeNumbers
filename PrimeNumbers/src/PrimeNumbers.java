5cf031aecfd5cf53712f4c3a021e15a08a1f1e2f
excipio85 <excipio85@gmail.com>
2015-01-17 16:10:03 +0100

    Initial checkin of prime numbers

---
+++ PrimeNumbers/src/PrimeNumbers.java
@@ -0,0 +1,31 @@
+
+public class PrimeNumbers {
+	
+	// keinen positiven Divisor 
+	
+	
+	public static void main(String[] args){
+		
+		
+		int limit = 100000;
+		int counter = 0;
+		boolean primzahl;
+		for(int zahl = 2; zahl <= limit; zahl++){
+			primzahl = true;
+			for(int zaehler = 2; zaehler <= zahl / 2 ; zaehler++){
+				if(zahl % zaehler == 0){
+					// primzahl gefunden
+					primzahl = false;
+					break;
+				}
+			}
+			if(primzahl){
+				counter++;
+				System.out.println(zahl +"");
+			}
+		}
+		System.out.println("Primzahlen: " + counter);
+		
+	}
+
+}
